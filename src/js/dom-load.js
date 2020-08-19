1751659d1af83fa503b76aaa6cfade330b190539
Jakub Ferenc <jakub@assemblage.cz>
2020-08-19 23:54:25 +0200

    Init commit

---
+++ src/js/dom-load.js
@@ -0,0 +1,18 @@
+import {
+  __removeClass,
+  __addClass,
+  __hasClass,
+  __toggleClass,
+} from './lib/utils';
+
+
+const domLoad = () => {
+
+  console.log('domLoad');
+
+  const d = document;
+
+
+};
+
+export default domLoad;
